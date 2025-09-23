b229d05a40a99074a1d6849fce1a25ed6237a05a
nemanjapetrasinovic <nemanjapet94@gmail.com>
2025-09-23 09:55:54 +0200

    Ignore node modules in telescope search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,12 @@ return {
         'nvim-telescope/telescope.nvim',
         name = 'telescope',
         config = function ()
+            require("telescope").setup({
+                defaults = {
+                    file_ignore_patterns = { "node_modules" },
+                },
+            })
+
             vim.keymap.set('n', '<leader>F', ':Telescope find_files<CR>')
             vim.keymap.set('n', '<leader>L', ':Telescope buffers<CR>')
             vim.keymap.set('n', '<leader>G', ':Telescope git_status<CR>')
@@ -10,3 +16,4 @@ return {
         end
     }
 }
+
